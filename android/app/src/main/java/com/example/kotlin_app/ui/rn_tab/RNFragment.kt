5f0ef3b94eb2c1f35186af68e4342d22546fab2e
Mishael Akinyemi <akinyemimosolasi@gmail.com>
2022-06-29 01:34:02 +0100

    chore: implement sharedViewModel and passing props from Native to RN

--- android/app/src/main/java/com/example/kotlin_app/ui/rn_tab/RNFragment.kt
+++ android/app/src/main/java/com/example/kotlin_app/ui/rn_tab/RNFragment.kt
@@ -2,20 +2,28 @@ package com.example.kotlin_app.ui.rn_tab
 
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin_app.BuildConfig
+import com.example.kotlin_app.data.models.RepoItem
+import com.example.kotlin_app.ui.native_tab.NativeViewModel
 import com.facebook.react.*
 import com.facebook.react.common.LifecycleState
 import com.facebook.soloader.SoLoader
+import com.example.kotlin_app.databinding.FragmentRnTabBinding
 
 
 class RNFragment : Fragment() {
-    private val mReactDelegate: ReactDelegate? = null
     private var mReactRootView: ReactRootView? = null
     private var mReactInstanceManager: ReactInstanceManager? = null
+    private val sharedViewModel: NativeViewModel by activityViewModels()
+    var initialProps = Bundle()
+    private var topRepo: RepoItem? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +32,16 @@ class RNFragment : Fragment() {
     ): View? {
         val application: Application = requireActivity().application
         SoLoader.init(requireContext(), false)
+
+        val sharedViewModel = ViewModelProvider(requireActivity()).get(NativeViewModel::class.java)
+        sharedViewModel.repos.observe(viewLifecycleOwner){
+                result ->
+            topRepo = result.data?.first()
+            initialProps.putString("user_avatar",topRepo?.owner?.avatar_url);
+            initialProps.putString("repo_title",topRepo?.name);
+            initialProps.putString("stars",topRepo?.stargazers_count?.toString());
+        }
+
 
         mReactRootView = ReactRootView(requireContext())
         val packages: List<ReactPackage> = PackageList(application).packages
@@ -37,7 +55,7 @@ class RNFragment : Fragment() {
             .setUseDeveloperSupport(BuildConfig.DEBUG)
             .setInitialLifecycleState(LifecycleState.RESUMED)
             .build();
-        mReactRootView!!.startReactApplication(mReactInstanceManager, "ReactNativeApp", null);
+        mReactRootView!!.startReactApplication(mReactInstanceManager, "ReactNativeApp", initialProps);
         return mReactRootView
     }
 
--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Image } from "react-native";
 
-const ReactNativeApp = () => {
+const HelloWorld = (props) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.hello}>Hello, World</Text>
+      <Image source={{ uri: props?.user_avatar }} style={styles.user_avatar} />
+      <Text style={styles.repoTitle}>{props?.repo_title || "No Data"}</Text>
+      <Text style={styles.repoStars}>Stars: {props?.stars}</Text>
     </View>
   );
 };
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "teal",
+  user_avatar: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+    resizeMode: "cover",
+    marginBottom: 20,
+    alignSelf: "center",
   },
-  hello: {
-    fontSize: 20,
+  repoStars: {
+    fontSize: 15,
     textAlign: "center",
     margin: 10,
   },
+  repoTitle: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    margin: 10,
+  },
+  container: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignSelf: "center",
+    backgroundColor: "#F5F5F5",
+  },
 });
 
-AppRegistry.registerComponent("ReactNativeApp", () => ReactNativeApp);
+AppRegistry.registerComponent("ReactNativeApp", () => HelloWorld);
